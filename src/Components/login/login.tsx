30dda34beb0c85ba7525a6cfb76afa42991cfe52
asd <asdf.com>
2025-09-19 11:51:31 +0200

    de bükszén

---
+++ src/Components/login/login.tsx
@@ -0,0 +1,18 @@
+import useState from 'react'
+import XIcon from '@mui/icons-material/X';
+const Login = () => {
+  return (
+   <>
+        <section>
+            <div>
+                 <XIcon/>
+            </div>
+            <aside>
+
+            </aside>
+        </section>
+   </> 
+  )
+}
+
+export default Login
